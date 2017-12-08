2b11c2c18956e082e85278389c32882b9d7fdf2a
e-ecano <oceanebertrand23@hotmail.fr>
2017-12-08 12:22:26 +0100

    My commit message

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <table class="grid">
+      <tr class="cell"
+          v-for="ligne in grille">
+        <td class="cell"
+            v-for="cellule in ligne"
+            :class="cellule.clique ? 'red' : 'green'"
+            @click="click(cellule)">{{cellule.bombe}}<bomb/></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+  import Bomb from './bomb'
+
+  export default {
+    name: 'App',
+    components: {Bomb},
+    data () {
+      const grille = []
+      // Lignes
+      for(var i = 0; i<10; i++){
+        // Cellules
+        var ligne = []
+        for(var j = 0; j<10; j++){
+          ligne.push({
+            bombe: true,
+            x: j,
+            y: i,
+            clique: false
+          })
+        }
+        grille.push(ligne)
+
+      }
+      return {
+        grille: grille
+      }
+    },
+    methods: {
+      click(cellule){
+        console.log(`x: ${cellule.x} - y:${cellule.y}`)
+        cellule.clique = true
+        //if (bomb.etat === false){
+          //console.log(`=== Il n'y a pas de bombe en `)
+        //}
+      },
+    }
+  }
+</script>
+
+<style>
+  @import "stylesheets/style.scss";
+</style>
---
+++ src/bomb.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="explode"
+            @contextmenu.prevent="turnOff"
+            class="bomb"
+            :etat="true"
+            >Q
+    </button>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'bomb',
+    props: {
+      bomb: {
+        required: false,
+        default: () => ({
+          etat: true
+        })
+      }
+    },
+    methods:{
+      explode(){
+        console.log("=== EXPLOSION ! ===")
+      }
+    }
+  }
+</script>
